a4964e0deffe152e4d0676b15b1631a85553af08
Tompriv <128976235+Tompriv@users.noreply.github.com>
2023-04-09 14:54:15 +0200

    Add files via upload

---
+++ AnonX/plugins/tomapi.py
@@ -0,0 +1,82 @@
+import asyncio
+import os
+
+from AnonX import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from AnonX import app
+import requests
+import pyrogram
+from pyrogram import Client, emoji 
+from config import *
+from pyrogram import filters
+from strings.filters import command
+from config import OWNER_ID
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from pyrogram.errors import MessageNotModified
+
+
+
+@app.on_message(
+    filters.command("cr")
+    & ~filters.private
+)
+async def cr_source(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://graph.org/file/c973727bddf803e028539.jpg",
+        caption=f"""**⩹━★⊷━⌞ 𝘾𝙍 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝━⊶★━⩺**\nمرحبا بك عزيزي {message.from_user.mention}\nانا بوت الذكاء الاصطناعي الخاص بسورس cr \nلمعرفة الاوامر اضغط على الأزرار بالأسفل👇\n**⩹━★⊷━⌞ 𝘾𝙍 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝━⊶★━⩺**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "طريقة الإستخدام", callback_data="usage"), 
+                 ],[
+                    InlineKeyboardButton(
+                        "𝙏َِ𝙊َِ𝙈ِ", url=f"https://t.me/DEV_TOM"),
+                    InlineKeyboardButton(
+                        "ρ᥆kᥱꪔ᥆ꪀ", url=f"https://t.me/devpokemon"),
+                ],[
+                
+                    InlineKeyboardButton(
+                        "★⌞ 𝘾𝙍 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝⚡", url=f"https://t.me/pp_g3"),
+                ],
+
+            ]
+
+        ),
+
+    )
+
+    
+@app.on_callback_query(filters.regex("usage"))
+async def cr_usage(_, callback_query: CallbackQuery):
+    await callback_query.answer()
+    await callback_query.message.edit_text(
+        text="""**⩹━★⊷⌯⌞ 𝘾𝙍 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝⌯⊶★━⩺**
+★¦ اهلا بك عزيزي في قسم الأوامر
+★¦ لتتمكن من تشغيل الذكاء الاصطناعي فقط اكتب
+★¦ /gpt - لـلـسـؤال آي سـؤال بالـذكـاء الاسـطـناعي
+
+**⩹━★⊷⌯⌞ 𝘾𝙍 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝⌯⊶★━⩺**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "العودة", callback_data="back"), 
+                ]
+            ]
+        )
+    )
+
+    
+@app.on_callback_query(filters.regex("back"))
+async def cr_back(_, callback_query: CallbackQuery):
+    message = callback_query.message
+  
+    await message.edit_reply_markup(reply_markup=InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton("طريقة الإستخدام", callback_data="tommm")],
+            [InlineKeyboardButton("𝙏َِ𝙊َِ𝙈ِ", url=f"https://t.me/DEV_TOM"),
+             InlineKeyboardButton("ρ᥆kᥱꪔ᥆ꪀ", url=f"https://t.me/devpokemon")],
+            [InlineKeyboardButton("★⌞ 𝘾𝙍 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝⚡", url=f"https://t.me/pp_g3")],
+        ]
+    ))
+
--- AnonX/plugins/تومي.py
+++ AnonX/plugins/تومي.py
@@ -6,19 +6,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 from pyrogram import Client, filters
 
-import asyncio
-import os
 
-from AnonX import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
-import requests
-import pyrogram
-from pyrogram import Client, emoji 
-from config import *
-from pyrogram import filters
-from strings.filters import command
-from config import OWNER_ID
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
-from pyrogram.errors import MessageNotModified
 
 url = 'https://us-central1-chat-for-chatgpt.cloudfunctions.net/basicUserRequestBeta'
 
@@ -59,56 +47,3 @@ def reply_gpt(client, message):
 
 
               
-@app.on_message(
-    filters.command("cr")
-    & ~filters.private
-)
-async def tom(client: Client, message: Message):
-    await message.reply_photo(
-        photo=f"https://graph.org/file/c973727bddf803e028539.jpg",
-        caption=f"""**⩹━★⊷━⌞ 𝘾𝙍 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝━⊶★━⩺**\nمرحبا بك عزيزي {message.from_user.mention}\n انا بوت الذكاء الاصطناعي الخاص بسورس cr \nلمعرفة الاوامر اضغط علي الازرار بالاسفل👇\n**⩹━★⊷━⌞ 𝘾𝙍 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝━⊶★━⩺**""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "طريقة الإستخدام", callback_data="tommm"), 
-                 ],[
-                    InlineKeyboardButton(
-                        "𝙏َِ𝙊َِ𝙈ِ", url=f"https://t.me/DEV_TOM"),
-                    InlineKeyboardButton(
-                        "ρ᥆kᥱꪔ᥆ꪀ", url=f"https://t.me/devpokemon"),
-                ],[
-                
-                    InlineKeyboardButton(
-                        "★⌞ 𝘾𝙍 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝⚡", url=f"https://t.me/pp_g3"),
-                ],
-
-            ]
-
-        ),
-
-    )
-
-
-
-@app.on_callback_query(filters.regex("tommm"))
-async def tommm(_, query: CallbackQuery):
-    await query.edit_message_text(
-        f"""**⩹━★⊷⌯⌞ 𝘾𝙍 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝⌯⊶★━⩺**
-       
-★¦ اهلا بك عزيزي في قسم الأوامر
-★¦ لتتمكن من تشغيل الذكاء الاصطناعي فقط اكتب
-★¦ /gpt - لـلـسـؤال آي سـؤال بالـذكـاء الاسـطـناعي
-
-**⩹━★⊷⌯⌞ 𝘾𝙍 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝⌯⊶★━⩺**""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "رجوع",
-                        switch_inline_query_current_chat="/cr"  
-                    ),
-                ],
-            ]
-        ),
-    )
