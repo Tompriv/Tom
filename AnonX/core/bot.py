e9e1db9dea0729eb253b668a5f2588fdee3d819a
Tompriv <128976235+Tompriv@users.noreply.github.com>
2023-04-11 19:14:30 +0200

    Update bot.py

--- AnonX/core/bot.py
+++ AnonX/core/bot.py
@@ -36,7 +36,7 @@ class AnonXBot(Client):
         LOGGER(__name__).info(f"MusicBot Started as {self.name}")
         try:
             await self.send_message(
-                config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :**\n\n✨ ɪᴅ : `{self.id}`\n❄ ɴᴀᴍᴇ : {self.name}\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.username}"
+                config.LOG_GROUP_ID, f"**»  تم تشغيل بوتك على 𝘾𝙍 • 𝙎𝙊𝙐𝙍𝘾𝙀  بنجاح :**\n\n✨ ɪᴅ : `{self.id}`\n❄ ɴᴀᴍᴇ : {self.name}\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.username}"
             )
         except:
             LOGGER(__name__).error(
