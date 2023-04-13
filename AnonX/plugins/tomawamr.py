2522ace0499ba688a26d32873864a696ec8202c3
Tompriv <128976235+Tompriv@users.noreply.github.com>
2023-04-14 01:00:19 +0200

    Update tomawamr.py

--- AnonX/plugins/tomawamr.py
+++ AnonX/plugins/tomawamr.py
@@ -16,7 +16,7 @@ from pyrogram.errors import MessageNotModified
 
 
 @app.on_message(
-    filters.command("الاوامر")
+    command("الاوامر")
 )
 async def cr_source(client: Client, message: Message):
     await message.reply_photo(
