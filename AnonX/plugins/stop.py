a5e79ad67b5582a2ae7f3202d7d2797698b516d6
Tompriv <128976235+Tompriv@users.noreply.github.com>
2023-04-14 00:40:38 +0200

    Add files via upload

--- AnonX/plugins/stop.py
+++ AnonX/plugins/stop.py
@@ -1,3 +1,13 @@
+#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from strings.filters import command
 from pyrogram import filters
 from pyrogram.types import Message
 
@@ -6,17 +16,17 @@ from strings import get_command
 from AnonX import app
 from AnonX.core.call import Anon
 from AnonX.utils.database import set_loop
-from AnonX.utils.decorators import AdminRightsCheck
-from AnonX.utils.decorators.adminss import AdminRightsCheckk
-from AnonX.utils.inline.play import close_keyboard
-from strings.filters import command
+from AnonX.utils.decorators import AdminRightsCheck, AdminRightsCheckCB
+
 # Commands
 STOP_COMMAND = get_command("STOP_COMMAND")
+STOP_COMMAND_chh = get_command("STOP_COMMAND_chh")
 
 
 @app.on_message(
     command(STOP_COMMAND)
     & filters.group
+    & ~filters.edited
     & ~BANNED_USERS
 )
 @AdminRightsCheck
@@ -26,22 +36,26 @@ async def stop_music(cli, message: Message, _, chat_id):
     await Anon.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
-        _["admin_9"].format(message.from_user.first_name),
-        reply_markup=close_keyboard,
+        _["admin_9"].format(message.from_user.mention)
     )
 
+
 @app.on_message(
-    command(STOP_COMMAND)
+    command(STOP_COMMAND_chh)
     & filters.channel
+    & ~filters.edited
     & ~BANNED_USERS
 )
-@AdminRightsCheckk
-async def stop_music(cli, message: Message, _, chat_id):
+@AdminRightsCheckCB
+async def stop_music_ch(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
     await Anon.stop_stream(chat_id)
     await set_loop(chat_id, 0)
+    if message.sender_chat:
+        mention = f'<a href=tg://user?id={message.chat.id}>{message.chat.title}</a>'
+    else:
+        mention = message.from_user.mention
     await message.reply_text(
-        _["admin_9"].format(message.from_user.first_name),
-        reply_markup=close_keyboard,
+        _["admin_9"].format(mention)
     )
