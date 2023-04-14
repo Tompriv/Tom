decf9a51e9560a6c4f07a55512e1b648e385de32
Tompriv <128976235+Tompriv@users.noreply.github.com>
2023-04-14 04:07:06 +0200

    Add files via upload

--- AnonX/plugins/vctools.py
+++ AnonX/plugins/vctools.py
@@ -1,89 +1,70 @@
 from random import randint
+from random import randint
 from typing import Optional
-
-from pyrogram import Client, enums, filters
+from AnonX import app
+from pyrogram import Client, filters
 from pyrogram.raw.functions.channels import GetFullChannel
 from pyrogram.raw.functions.messages import GetFullChat
 from pyrogram.raw.functions.phone import CreateGroupCall, DiscardGroupCall
 from pyrogram.raw.types import InputGroupCall, InputPeerChannel, InputPeerChat
 from pyrogram.types import Message
-from pyrogram.types import Message
-from AnonX import app
 
-
-def get_arg(message: Message):
-    msg = message.text
-    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
-    split = msg[1:].replace("\n", " \n").split(" ")
-    if " ".join(split[1:]).strip() == "":
-        return ""
-    return " ".join(split[1:])
 
 async def get_group_call(
     client: Client, message: Message, err_msg: str = ""
 ) -> Optional[InputGroupCall]:
     chat_peer = await client.resolve_peer(message.chat.id)
-    if isinstance(chat_peer, (InputPeerChannel, InputPeerChat)):
-        if isinstance(chat_peer, InputPeerChannel):
-            full_chat = (
-                await client.invoke(GetFullChannel(channel=chat_peer))
-            ).full_chat
-        elif isinstance(chat_peer, InputPeerChat):
-            full_chat = (
-                await client.invoke(GetFullChat(chat_id=chat_peer.chat_id))
-            ).full_chat
-        if full_chat is not None:
-            return full_chat.call
-    await message.edit(f"**No group call Found** {err_msg}")
-    return False
+    if isinstance(chat_peer, InputPeerChannel):
+        full_chat = (
+            await client.invoke(GetFullChannel(channel=chat_peer))
+        ).full_chat
+    elif isinstance(chat_peer, InputPeerChat):
+        full_chat = (
+            await client.invoke(GetFullChat(chat_id=chat_peer.chat_id))
+        ).full_chat
+    else:
+        full_chat = None
+        
+    if full_chat is not None:
+        return full_chat.call
+
+    await message.reply_text(f"**No group call found** {err_msg}")
+    return None
 
 
 @app.on_message(
-    filters.command(["فتح الكول"], ".") & (filters.me)
+    filters.command("فتح الكول")
+    & filters.me
 )
 async def opengc(client: Client, message: Message):
-    flags = " ".join(message.command[1:])
-    tex = await message.reply_text(message, "`Processing . . .`")
-    vctitle = get_arg(message)
-    if flags == enums.ChatType.CHANNEL:
-        chat_id = message.chat.title
-    else:
-        chat_id = message.chat.id
-    args = f"**تم بداء المحادثه\n ⌔ **Chat ID** : `{chat_id}`"
+    args = message.text.split(" ", 1)
+    chat_id = message.chat.title if len(args) == 1 else args[1]
+    vctitle = None if len(args) == 1 else args[1]
+    
+    tex = await message.reply_text("Processing...")
+    
     try:
-        if not vctitle:
-            await client.invoke(
-                CreateGroupCall(
-                    peer=(await client.resolve_peer(chat_id)),
-                    random_id=randint(10000, 999999999),
-                )
+        await client.invoke(
+            CreateGroupCall(
+                peer=await client.resolve_peer(chat_id),
+                random_id=randint(10000, 999999999),
+                title=vctitle,
             )
-        else:
-            args += f"\n ⌔ **Title:** `{vctitle}`"
-            await client.invoke(
-                CreateGroupCall(
-                    peer=(await client.resolve_peer(chat_id)),
-                    random_id=randint(10000, 999999999),
-                    title=vctitle,
-                )
-            )
-        await tex.edit(args)
+        )
+        await tex.edit(f"**Started group call**\n**Chat ID:** `{chat_id}`\n**Title:** `{vctitle}`" if vctitle else f"**Started group call**\n**Chat ID:** `{chat_id}`")
     except Exception as e:
-        await tex.edit(f"**INFO:** `{e}`")
+        await tex.edit(f"**Error:** `{str(e)}`")
 
 
 @app.on_message(
-    filters.command(["قفل الكول"], ".") & (filters.me)
+    filters.command("قفل الكول")
+    & filters.me
 )
-async def end_vc_(client: Client, message: Message):
-    chat_id = message.chat.id
-    if not (
-        group_call := (
-            await get_group_call(client, message, err_msg=", group call already ended")
-        )
-    ):
+async def closegc(client: Client, message: Message):
+    group_call = await get_group_call(client, message, ", group call already ended")
+    
+    if group_call is None:
         return
+    
     await client.invoke(DiscardGroupCall(call=group_call))
-    await message.reply_text(f"تم قفل المحادثه في **Chat ID** : `{chat_id}`")
-
-
+    await message.reply_text("**Ended group call**")
