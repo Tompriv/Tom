7356d09ec38a34a5d88a25913499098b63498133
Tompriv <128976235+Tompriv@users.noreply.github.com>
2023-04-17 07:07:55 +0200

    Add files via upload

---
+++ AnonX/plugins/c as tom.py
@@ -0,0 +1,23 @@
+from pyrogram import filters, Client
+from AnonX import app
+import asyncio
+from pytgcalls import PyTgCalls, StreamType
+from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
+from AnonX.core.call import Anon
+from AnonX.utils.database import *
+from pytgcalls.exceptions import NoActiveGroupCall, TelegramServerError
+
+@app.on_message(filters.regex("تغيير اسم المساعد"))
+async def tom_name(client, message):
+    assistant = await group_assistant(Anon, message.chat.id)
+    await message.reply("ارسل اسم المساعد الجديد:")
+    try:
+        msg = await app.listen(message.chat.id)
+        new_name = msg.text
+        await client.update_profile(first_name=new_name)
+        await message.reply(f"تم تغيير اسم المساعد الى {new_name}")
+    except Exception as e:
+        print(str(e))
+        await message.reply("حدث خطأ اثناء تغيير اسم المساعد!")
+
+
